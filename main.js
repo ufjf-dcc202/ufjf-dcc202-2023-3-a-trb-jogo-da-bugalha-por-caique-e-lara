840cc9d5a73540a2aca7a50acf513631f5ebca88
Lara Dias <lara.dias@estudante.ufjf.br>
2023-12-06 19:10:41 -0300

    cria script main.js com as funções do dado e dado.css para estilização do dao

--- main.js
+++ main.js
@@ -1,4 +1,44 @@
-function IniciarJogo(){
+//Identificar elementos
+let numeroSorteado = document.querySelector('#Dado');
+const imgDado = document.querySelector('#imgDado');
+const bntJogar = document.querySelector('#btnJogar');
+const sorteado = document.querySelector('#sorteado');
 
-    const randomNumber = Math.floor(Math.random() * 6) + 1;
+bntJogar.addEventListener('click', sortear);
+
+function sortear() {
+    //animacao
+    imgDado.classList.add('animar');
+    sorteado.classList.add('aparecer');
+    //esconde botao
+    bntJogar.style.display = 'none';
+
+    //executa as ações depois de determinado tempo
+    setTimeout(function animacao() {
+        //armazena numero sorteado
+        numeroSorteado = getRandomInt(1,6);
+        //numero sorteado aparece no console
+        console.log(numeroSorteado);
+
+        //mostra imagem com base no numero
+        imgDado.setAttribute('src', 'imagens/'+numeroSorteado+'.png');
+
+        //exibe botao
+        bntJogar.style.display = 'inline-block';
+
+        //retira animacao
+        imgDado.classList.remove('animar');
+        imgDado.classList.remove('aparecer');
+        
+    }, 1750);
+
+}
+
+function getRandomInt(min, max) {
+    //arredonda para cima 
+    min = Math.ceil(min);
+    //arredonda para baixo
+    max = Math.floor(max);
+
+    return Math.floor(Math.random()*(max - min + 1)) + min;
 }
